5507872848bb5af7ab3e8248d4df9515576afcab
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-30 13:08:04 -0700

    main: Get event listener firing

--- src/js/components/switcher.js
+++ src/js/components/switcher.js
@@ -1,1 +1,10 @@
-console.log(`Working`);
+const switcher = document.querySelector("[data-component='switcher']");
+const switcherHeader = document.querySelector("[data-component='switcher'] [data-target='header']");
+
+const toggleClass = (event) => {
+  console.log(event);
+  switcherHeader.classList.toggle("LUNA");
+  
+}
+
+switcherHeader.addEventListener("click", toggleClass);
