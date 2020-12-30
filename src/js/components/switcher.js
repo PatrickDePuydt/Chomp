acfb7f4c5af9ae4382bf95b1038cbaab4d604f74
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-30 13:12:54 -0700

    main: Widen event handler jurisdiction

--- src/js/components/switcher.js
+++ src/js/components/switcher.js
@@ -1,10 +1,17 @@
 const switcher = document.querySelector("[data-component='switcher']");
 const switcherHeader = document.querySelector("[data-component='switcher'] [data-target='header']");
+const lunaSidebar = document.querySelector("[data-component='switcher'] [data-kitten='LUNA']");
+const lilySidebar = document.querySelector("[data-component='switcher'] [data-kitten='LILY']");
+
+const modifyHeader = () => switcherHeader.classList.toggle("SWITCH");
+const swellSidebar = () => lunaSidebar.classList.toggle("SWITCH");
+const recedeSidebar = () => lilySidebar.classList.toggle("SWITCH");
 
 const toggleClass = (event) => {
-  console.log(event);
-  switcherHeader.classList.toggle("LUNA");
-  
+  modifyHeader();  
+  swellSidebar();  
+  recedeSidebar();  
 }
+
 
 switcherHeader.addEventListener("click", toggleClass);
