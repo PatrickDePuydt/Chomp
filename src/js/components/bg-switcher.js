2a30884112d0bdc73b066e24d25455571c7afcf4
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-01 15:46:31 -0700

    main: Remove console logs

--- src/js/components/bg-switcher.js
+++ src/js/components/bg-switcher.js
@@ -1,7 +1,4 @@
 const swapBackground = (piecePower, playerInventory) => {
  let element = document.querySelector(`[data-family='x${piecePower}']`);
-//  console.log(element);
-  // element.target.innerText
-  console.log(`x-${piecePower}-${playerInventory}`);
-  element.classList.add(`x-${piecePower}-${playerInventory}`);
+ element.classList.add(`x-${piecePower}-${playerInventory}`);
 }
