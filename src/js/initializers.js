d2c29010b53aa35ddb7113765f78c61391be5477
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-01 17:40:44 -0700

    main: Rename file, transpile class inline declaration into initial state function

--- src/js/initializers.js
+++ src/js/initializers.js
@@ -9,6 +9,10 @@ let opposingTeam; // Used for turn tracking
 
 const assignPieceEventHandlers = () => [...gamePieces].map( piece => piece.addEventListener('click', handlePieceSelectionClick)); // Assign events listeners to all the pieces
 const assignCellEventHandlers = () => [...gameBoardCells].map( cell => cell.addEventListener('click', handleCellClick)); // Add events to all the cells
+const setInitialClassState = () => [...gamePieces].map( piece => {
+  piece.classList.add("x-3-3");
+}); // Add events to all the cells
+
 const disableBoard = () => {gameBoardCells.forEach( cell => cell.setAttribute("disabled", true))}; // Used to lock the board
 const enableBoard = () => {gameBoardCells.forEach( cell => cell.setAttribute("disabled", false))}; // Used to unlock the board
 const handleWin = () => console.log(`Win`); // Used as a placeholder for win tracking
@@ -16,3 +20,4 @@ const handleDraw = () => console.log(`draw`); // Used as a placeholder for draw 
 const isDraw = () => [...gameBoardCells].every( cell => cell.classList.contains("LILY") || cell.classList.contains("LUNA")); // used to check if there was a combination (used in win evaluation)
 const resetPiecePower = () => console.log(`Reset`); // Utility for clean piece tracking
 const setLayout = () => switchClass(currentTeam, opposingTeam); // Sets the layout of the board
+
